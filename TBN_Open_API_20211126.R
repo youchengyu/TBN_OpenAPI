99adc91d2d8cf2c393a6919d60e2b83cf6f76dea
youchengyu <vincent199321@gmail.com>
2021-11-30 17:23:26 +0800

    Create TBN_Open_API_20211126.R
    upload R function : TBN_OpenAPI   (last modified 2021-11-26)

---
+++ TBN_Open_API_20211126.R
@@ -0,0 +1,34 @@
+############################
+# TBN OpenAPI version 2.01 #
+# author: Vincent yu       #
+# update: 2021-11-26       #
+############################
+
+# API documents: https://www.tbn.org.tw/data/api/openapi/v2
+# A way to download TBN Open API in r
+
+library(tidyverse)
+library(jsonlite)
+library(progress)
+
+TBN_OpenAPI <- function(...,version="v2",type="occurrence",message=TRUE){
+  occ_api <-jsonlite::fromJSON(paste0("https://www.tbn.org.tw/api/",version,"/",type,"?",...))
+  page <- list()
+  page[[1]] <- occ_api %>% .$data
+  pg <- (occ_api %>% .$meta %>% .$total/300) %>% ceiling()
+  pb <- progress_bar$new(total = pg-1)
+  if(pg>1){
+    for ( i in 2:pg) { #download and combine data
+      occ_api <- occ_api %>% .$links %>% .$`next` %>% jsonlite::fromJSON()
+      pb$tick()
+      page[[i]] <- occ_api %>% .$data
+    }
+    result <- jsonlite::rbind_pages(page) 
+    message("type= ", type, ", version= ", version, " ",dim(result)[1],"data downloaded")
+    return(result)
+  }
+  else{
+    message("type= ", type, ", version= ", version, " ",dim(result)[1],"data downloaded")
+    return(occ_api %>% .$data)
+  }
+}
