5fc95027903167d2319a43416c01b4af0f07d64d
youchengyu <vincent199321@gmail.com>
2022-03-23 18:29:46 +0800

    update to v25

---
+++ TBN_Open_2.5API_20220316.R
@@ -0,0 +1,38 @@
+############################
+# TBN OpenAPI version 2.5 #
+# author: Vincent yu       #
+# update: 2022-03-17       #
+############################
+
+# API documents: https://www.tbn.org.tw/data/api/openapi/v2
+# A way to download TBN Open API in r
+
+library(tidyverse)
+library(jsonlite)
+library(progress)
+
+TBN_OpenAPIv25 <- function(...,version="v25",type="occurrence",message=TRUE){
+  occ_api <-jsonlite::fromJSON(paste0("https://www.tbn.org.tw/api/v25/",type,"?",...,"&limit=3000"))
+  page <- list()
+  page[[1]] <- occ_api %>% .$data
+  if (occ_api %>% .$meta %>% .$status == "NOT FOUND"){
+    message("type= ", type, ", version= ", version, " ","No data downloaded")
+  } else if(occ_api %>% .$meta %>% .$status == "SUCCESS"){
+    pg <- (occ_api %>% .$meta %>% .$total/3000) %>% ceiling()
+    pb <- progress_bar$new(total = pg-1)
+    if(pg>1){
+      for ( i in 2:pg) { #download and combine data
+        occ_api <- occ_api %>% .$links %>% .$`next` %>% jsonlite::fromJSON()
+        pb$tick()
+        page[[i]] <- occ_api %>% .$data
+      }
+      result <- jsonlite::rbind_pages(page) 
+      message("type= ", type, ", version= ", version, " ",dim(result)[1],"data downloaded")
+      return(result)
+    }
+    else {
+      message("type= ", type, ", version= ", version, " ",occ_api %>% .$meta %>% .$total," data downloaded")
+      return(occ_api %>% .$data)
+    }
+  }
+}
